0b41c7c453ede193da9a3d90964747ae0a1c20c5
Gonzalo de Antonio <47611628+gonibix23@users.noreply.github.com>
2022-05-12 19:53:23 +0200

    Error subiendo archivos

--- AbstractFactory/Prueba.java
+++
@@ -1,9 +0,0 @@
-package AbstractFactory;
-
-public class Prueba {
-	public static void main(String argv[]) {
-		EnemyFactory fabrica1 = new EnemiesWorld1Hell();
-		Enemy esqueleto = fabrica1.createSkeleton();
-		esqueleto.attack();
-	}
-}
