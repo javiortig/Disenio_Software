9dfbf02fcea6081ed274002cf484466c8c48f1a0
Gonzalo de Antonio <47611628+gonibix23@users.noreply.github.com>
2022-05-12 17:29:44 +0200

    Add files via upload

--- AbstractFactory/EnemyFactory.java
+++ AbstractFactory/EnemyFactory.java
@@ -1,10 +1,19 @@
 package AbstractFactory;
 
+import java.util.Random;
+
 public abstract class EnemyFactory {
 	public abstract Slime createSlime();
 	public abstract Skeleton createSkeleton();
 	public abstract Zombie createZombie();
 	public final Enemy randomEnemy() {
-		
+		Random rand = new Random();
+		int enemyType = rand.nextInt(2);
+		if(enemyType == 0)
+			return createSlime();
+		else if(enemyType == 1)
+			return createSkeleton();
+		else
+			return createZombie();
 	}
 }
