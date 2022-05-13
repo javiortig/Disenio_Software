5cc5653b851d6033f041000fed4a928163925721
José Delgado <72045733+hypermoderngalois@users.noreply.github.com>
2022-05-13 09:28:04 +0200

    Update Singleton.java
    Crear función calcularDamage().

--- Singleton/Singleton.java
+++ Singleton/Singleton.java
@@ -1,28 +1,30 @@
 package Singleton;
 
-import Game.Player;
-import Game.Enemy;
-
 public class Singleton {
 	
 	private static Singleton singleton;
 	
 	// El constructor es privado, así no permite que se genere un constructor por defecto
-	private Singleton(Player jugador, Enemy enemigo) {
+	private Singleton() {
 		// Aquí simplemente realizamos la evaluación de los daños
 	}
 	
-	public static Singleton getSingletonInstance(Player jugador, Enemy enemigo) {
+	public static Singleton getSingletonInstance() {
 		if(singleton == null) {
-			singleton = new Singleton(jugador, enemigo);
+			singleton = new Singleton();
 		} else {
 			System.out.println("No se puede crear el objeto porque ya exite un objeto de la clase Singleton.");
 		}
 		return singleton;
 	}
 	
-	// Métodos getters y setters
-
+	// Métodos
+	public int calcularDamage(int daño, int defensa, boolean defendiendo) {
+		int damage;
+		if(defendiendo) damage = daño - defensa;
+		else damage = daño;
+		return damage;
+	}
 
 	// Podemos sobrescribr el método clone, para que no se pueda clonar
 	// un objeto de esta clase
